b3a4fdd9e39cba5d702f271864f23a4241cabc93
Jessica <jessica.ju@stonybrook.edu>
2018-04-30 14:59:50 -0400

    Added new branch

--- app/src/main/java/com/example/kevin/limtreasurehunt/MainActivity.java
+++ app/src/main/java/com/example/kevin/limtreasurehunt/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.kevin.limtreasurehunt;
 
+import android.content.Intent;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
+import android.media.MediaRecorder;
+import android.speech.SpeechRecognizer;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaRecorder mmediaRecorder;
+    private SpeechRecognizer sr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,4 +34,63 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void goToFirst(View v) { setContentView(R.layout.screen3);}
+
+    public void goToSecond(View v) { setContentView(R.layout.screen4);}
+
+    public void voiceRecognize(View v){
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        RecognitionListener listener = new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle params) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+
+            }
+
+            @Override
+            public void onRmsChanged(float rmsdB) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] buffer) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int error) {
+
+            }
+
+            @Override
+            public void onResults(Bundle results) {
+
+            }
+
+            @Override
+            public void onPartialResults(Bundle partialResults) {
+
+            }
+
+            @Override
+            public void onEvent(int eventType, Bundle params) {
+
+            }
+        };
+
+        sr = SpeechRecognizer.createSpeechRecognizer(this);
+        sr.setRecognitionListener((RecognitionListener) listener);
+        sr.startListening(intent);
+
+        System.out.print("I'm listening!");
+
+    }
 }
