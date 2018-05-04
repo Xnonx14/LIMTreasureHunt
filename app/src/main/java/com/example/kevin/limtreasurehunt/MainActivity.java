88c9fd9498c014dea6338e227e770af4f9897d49
Jessica <jessica.ju@stonybrook.edu>
2018-05-04 16:13:22 -0400

    Added speech recognition functionality.

--- app/src/main/java/com/example/kevin/limtreasurehunt/MainActivity.java
+++ app/src/main/java/com/example/kevin/limtreasurehunt/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.kevin.limtreasurehunt;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
@@ -11,15 +12,59 @@ import android.media.MediaRecorder;
 import android.speech.SpeechRecognizer;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     private MediaRecorder mmediaRecorder;
+    private Button speechButton;
     private SpeechRecognizer sr;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        speechButton = (Button)findViewById(R.id.button7)
+
+        speechButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                askSpeechInput();
+            }
+        });
+    }
+
+    private void askSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "Hi speak something");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    speechButton.setText(result.get(0));
+                }
+                break;
+            }
+        }
     }
 
     public void buttonOnClick(View v){
